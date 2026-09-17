4c08a133811a04ad0d33e5ad8297e42e483918b8
Monty Sylvan <193113132+MSylvan4376@users.noreply.github.com>
2026-09-16 22:27:21 -0700

    security: remove public SSH access

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -46,14 +46,6 @@ resource "aws_security_group" "ec2_sg" {
     security_groups = [aws_security_group.alb_sg.id]
   }
 
-  ingress {
-    description = "SSH from allowed CIDR"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.allowed_ssh_cidr]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
