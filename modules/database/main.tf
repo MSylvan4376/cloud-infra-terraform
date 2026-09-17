504355b57a902b49903e2de765ea1a49d91767a2
Monty Sylvan <193113132+MSylvan4376@users.noreply.github.com>
2026-09-16 23:11:53 -0700

    security: require TLS for RDS connections

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -33,6 +33,20 @@ resource "aws_security_group" "db_sg" {
   }
 }
 
+resource "aws_db_parameter_group" "this" {
+  name   = "${var.project_name}-${var.environment}-mysql8"
+  family = "mysql8.0"
+
+  parameter {
+    name  = "require_secure_transport"
+    value = "1"
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-mysql8"
+    Environment = var.environment
+  }
+}
 resource "aws_db_instance" "this" {
   identifier        = "${var.project_name}-${var.environment}-db"
   engine            = "mysql"
@@ -45,6 +59,7 @@ resource "aws_db_instance" "this" {
   manage_master_user_password = true
 
   db_subnet_group_name   = aws_db_subnet_group.this.name
+  parameter_group_name   = aws_db_parameter_group.this.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
 
   publicly_accessible = false
